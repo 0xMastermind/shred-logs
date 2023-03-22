02ffab26bfa2f88d36b8806b6eab81ee82b3c887
0xMastermind <127721218+0xMastermind@users.noreply.github.com>
2023-03-22 18:53:24 +0800

    update 1.0

--- shred_logs.sh
+++ shred_logs.sh
@@ -11,17 +11,21 @@ shred -uzn 5 /var/log/auth.log /var/log/lastlog /var/log/wtmp /var/log/btmp || {
     echo "Error: Failed to shred system log files"
     exit 1
 }
+echo "Successfully shredded system log files."
 
 # Shred root user's bash history
 shred -uzn 5 ~/.bash_history || {
     echo "Error: Failed to shred root user's bash history"
     exit 1
 }
+echo "Successfully shredded root user's bash history."
 
 # Shred all users' bash history
 find /home -type f -name '.bash_history' -exec shred -uzn 5 {} \; || {
     echo "Error: Failed to shred all users' bash history"
     exit 1
 }
+echo "Successfully shredded all users' bash history."
+
 
 
